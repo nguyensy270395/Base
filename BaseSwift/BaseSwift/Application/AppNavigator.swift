5add5ef5d6693fa13e4c6a94a32b739428d93947
nguyensy270395 <99628647+nguyensy270395@users.noreply.github.com>
2022-08-05 10:29:06 +0700

    create repo

---
+++ BaseSwift/BaseSwift/Application/AppNavigator.swift
@@ -0,0 +1,41 @@
+//
+//  AppNavigator.swift
+//  daibet_ios
+//
+//  Created by duchung on 14/02/2022.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+protocol AppNavigatorType: class {
+    var window: UIWindow? { get }
+    func start()
+    func switchTo(viewController: UIViewController)
+    func handleUrl(url: URL)
+}
+
+final class AppNavigator: AppNavigatorType {
+    var window: UIWindow?
+    
+    init(window: UIWindow?) {
+        self.window = window
+    }
+    
+    func start() {
+      
+    }
+    
+    func handleUrl(url: URL) {
+    }
+    
+    func switchTo(viewController: UIViewController) {
+        guard let window = window else { return }
+        window.rootViewController = viewController
+        window.makeKeyAndVisible()
+        UIView.transition(with: window, duration: 0.3, options: [.transitionCrossDissolve],
+                          animations: {}, completion: {completed in
+        })
+    }
+}
---
+++ BaseSwift/BaseSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseSwift
+//
+//  Created by Nguyễn Thanh Sỹ on 05/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
